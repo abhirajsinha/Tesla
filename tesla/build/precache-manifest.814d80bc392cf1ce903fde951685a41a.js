cc627cea9aaa0e2f0f67af04b10cdce54d43e4c8
Abhiraj <abhirajsinha25@gmail.com>
2023-02-01 11:46:12 +0530

    add build

---
+++ tesla/build/precache-manifest.814d80bc392cf1ce903fde951685a41a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b5377311c46f7bf982ab8733e7ae1d",
+    "url": "/tesla%20/index.html"
+  },
+  {
+    "revision": "7cbcdff4f878087f83af",
+    "url": "/tesla%20/static/css/main.a52bfb84.chunk.css"
+  },
+  {
+    "revision": "58bd9f27aa0fc1719db9",
+    "url": "/tesla%20/static/js/2.6011ffc9.chunk.js"
+  },
+  {
+    "revision": "b30eef99e3a387b570de839353cc6064",
+    "url": "/tesla%20/static/js/2.6011ffc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113c93042097a8d9d0af",
+    "url": "/tesla%20/static/js/3.93a59b92.chunk.js"
+  },
+  {
+    "revision": "7cbcdff4f878087f83af",
+    "url": "/tesla%20/static/js/main.97dae4a7.chunk.js"
+  },
+  {
+    "revision": "5a1db3e7471f6c8516f6",
+    "url": "/tesla%20/static/js/runtime-main.dfbe1be8.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "/tesla%20/static/media/logo.be622899.svg"
+  }
+]);
