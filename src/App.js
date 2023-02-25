7418d7c1d515ad662b6b027f2c78940607463c47
Abhiraj <abhirajsinha25@gmail.com>
2023-02-25 12:49:41 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Section from "./components/Section";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/README.md
@@ -0,0 +1,11 @@
+# Designed Tesla Website
+
+## Tech Stacks Used:
+1. React JS
+2. Redux
+3. Styled Components
+
+
+![Model S](../tesla/Red.png)
+![Model Y](../tesla/Blue.png)
+![Model X](../tesla/white.png)
